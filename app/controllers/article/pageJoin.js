f5d0cc4710e9cb5b5d5e9adf37c91ed4a25520da
Dca <dca35311@gmail.com>
2014-11-18 20:14:01 +0800

    很髒的設定隊伍上限5人

--- app/controllers/article/pageJoin.js
+++ app/controllers/article/pageJoin.js
@@ -3,7 +3,16 @@ var db = require('../../models');
 
 module.exports = function (req, res, next) {
 
-    var conditions = { _id: req.params.id };
+    /**
+    * work around
+    * 這裏很髒的限定組隊人數不能超過5人
+    *
+    * TODO 找出更靈活的解法
+    */
+    var conditions = {
+        _id: req.params.id,
+        $where: 'this.members.length < 6'
+    };
 
     db.Article
         .findOneAndUpdate(conditions, {
@@ -13,15 +22,15 @@ module.exports = function (req, res, next) {
         })
         .exec(function (err, doc) {
 
-            if (err) {
-                console.log(err.message);
-                console.log(err.stack);
+            if (err || !doc) {
+                console.log(err&&err.message);
+                console.log(err&&err.stack);
 
-                req.flash('error', '更新失敗');
-                res.redirect('/articles' + req.params.id);
+                req.flash('error', '加入失敗');
+                res.redirect('/articles/' + req.params.id);
                 return;
             }
-            req.flash('info', '更新成功');
+            req.flash('info', '加入成功');
             res.redirect('/articles/' + doc._id);
         });
 };
