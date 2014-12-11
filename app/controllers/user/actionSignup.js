b71c2f21dcf7053eae9db4ed51df9dabc6113c09
Your Name <catxii@gmail.com>
2014-12-11 13:41:46 +0800

    add github signup & signin

--- app/controllers/user/actionSignup.js
+++ app/controllers/user/actionSignup.js
@@ -4,33 +4,78 @@ var crypto = require('crypto');
 var config = require('../../../config/config');
 var db = require('../../models');
 
+var GitHubApi = require("github");
+
 module.exports = function (req, res, next) {
 
-    if (req.body.password !== req.body.password_confirmation) {
-        req.flash('error', '兩次密碼不一致!');
-        res.redirect('/signup');
-        return;
-    }
+    db.User
+    .findOne()
+    .where('name', req.body.name)
+    .exec(function(err, user){
+        if (err) {
+            return done(null, false, {message: '註冊失敗.'});
+        }
+        if (user) {
+            req.flash('error', '帳號已存在!');
+            res.redirect('/signup');
+            return;
+        }
 
-    var data = _.pick(req.body, [
-        'name',
-        'email',
-        'password'
-    ]);
-
-    data.email = data.email.trim().toLowerCase();
-    data.password = crypto.createHash('sha1').update(data.password + config.hashScrect.pwd).digest('hex');
-    data.gravatarHash = crypto.createHash('md5').update(data.email).digest('hex');
-
-    return db.User
-        .create(data, function(err, doc){
-            if (err) {
-                console.log('error', err);
-                req.flash('error', err);
-                res.redirect('/signup');
-                return;
-            }
-            res.redirect('/me');
+        // 定義
+        var github = new GitHubApi({
+            // required
+            version: "3.0.0",
+            // optional
+            debug: true,
+            protocol: "https",
+            host: "api.github.com",
+            // pathPrefix: "/api/v3", // for some GHEs
+            timeout: 5000,
         });
 
+        // 驗證
+        github.authenticate({
+            type: "basic",
+            username: req.body.name,
+            password: req.body.password
+        });
+
+        // 2.Create authorization get token
+        github.authorization.create({
+            scopes: ["gist", "repo", "user"],
+            note: "Hangee JoinNow",
+        }, function(githubErr, githubRes) {
+            if(githubRes == undefined) {
+                req.flash('error', 'Github 驗證失敗!');
+                res.redirect('/signup');
+                return;
+            } else {
+                var token = githubRes.token;
+                console.log(githubRes);
+
+                var data = _.pick(req.body, [
+                    'name',
+                    'email',
+                    'password',
+                    'githubToken'
+                ]);
+
+                data.email = data.email.trim().toLowerCase();
+                data.password = crypto.createHash('sha1').update(data.password + config.hashScrect.pwd).digest('hex');
+                data.gravatarHash = crypto.createHash('md5').update(data.email).digest('hex');
+                data.githubToken = token;
+
+                return db.User
+                    .create(data, function(err, doc){
+                        if (err) {
+                            console.log('error', err);
+                            req.flash('error', err);
+                            res.redirect('/signup');
+                            return;
+                        }
+                        res.redirect('/me');
+                    });
+            }
+        });
+    });
 };
--- app/middlewares/strategies/local.js
+++ app/middlewares/strategies/local.js
@@ -6,31 +6,68 @@ var crypto = require('crypto');
 var config = require('../../../config/config');
 var db = require('../../models');
 
+var GitHubApi = require("github");
 
 module.exports = function (app, config) {
 
     passport.use(new LocalStrategy({
-        usernameField: 'email'
-    }, function (email, password, done) {
-        debug('嘗試登入 %s', email);
+        usernameField: 'name'
+    }, function (name, password, done) {
+        debug('嘗試登入 %s', name);
         debug('嘗試登入 %s', password);
-        console.log('LocalStrategy', email, password);
+        console.log('LocalStrategy', name, password);
 
-        email = email.trim().toLowerCase();
-        password = crypto.createHash('sha1').update(password + config.hashScrect.pwd).digest('hex');
+        // 定義
+        var github = new GitHubApi({
+            // required
+            version: "3.0.0",
+            // optional
+            debug: true,
+            protocol: "https",
+            host: "api.github.com",
+            timeout: 5000,
+        });
+        
+        github.authenticate({
+            type: "basic",
+            username: name,
+            password: password
+        });
+
+        // github.authorization.get({
+        // }, function(err, res) {
+        //     console.log(JSON.stringify(res));
+        // });
+
+        // password = crypto.createHash('sha1').update(password + config.hashScrect.pwd).digest('hex');
 
         return db.User
             .findOne()
-            .where('email', email)
-            .where('password', password)
+            .where('name', name)
             .exec(function(err, user){
                 if (err) {
                     return done(null, false, {message: '登入失敗.'});
                 }
                 if (!user) {
-                    return done(null, false, {message: '請確認信箱和密碼正確.'});
+                    return done(null, false, {message: '請確認帳號正確.'});
                 }
-                done(null, user);
+
+                if(user.githubToken) {
+                    github.authorization.getAll({
+                        // user: name
+                    }, function(err, res) {
+                        console.log(JSON.stringify(res));
+                        // Github 驗證未通過
+                        if(res == undefined) {
+                            return done(null, false, {message: '請確認密碼正確.'});
+                        } else {
+                            console.log(user);
+                            done(null, user);
+                        }
+                    });
+                } else {
+                    return done(null, false, {message: '未驗證 Github Token.'});
+                }
             });
     }));
 
